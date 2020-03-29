8c56d68d1a898cb58a81511a61c1bd9f2c3c3bab
Lino Gabriel <60333772+LinoGabriel@users.noreply.github.com>
2020-03-29 14:09:35 -0300

    Add files via upload

---
+++ cadastro01.py
@@ -0,0 +1,27 @@
+from interface import *
+from arquivo import *
+arq = 'CadastroAJN.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair'])
+    if resposta == 1:
+        #Opção para ler um arquivo e ver os alunos cadastrados
+        cabecalho('\033[33mALUNOS REGISTRADOS\033[m'.center(40))
+        try:
+            lerArquivo(arq)
+        except:
+            print('\033[31mERRO AO LER OS DADOS DO ARQUIVO!!\033[m')
+    elif resposta == 2:
+        cabecalho('NOVO CADASTRO'.center(40))
+        nome = str(input('Nome: ')).strip().capitalize()
+        idade = leiaInt('Idade: ')
+        turma = str(input('Qual turma o aluno pertence? ')).strip().capitalize()
+        escreverArquivo(arq, nome, idade, turma)
+
+    elif resposta == 3:
+        print('sair do programa.')
+        break
+    else:
+        print(f'\033[31mA OPÇÃO {resposta} É INVALIDA, INSIRA UM VALOR VALIDO.\033[m')
